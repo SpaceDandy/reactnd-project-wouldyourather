eca59f636dc27f51838e9abb16c6b39f0f66e850
Victor Trevino <victortrevino448@gmail.com>
2019-08-14 13:17:49 -0700

    Adds Navlinks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom';
 
 class UserCard extends Component {
 
     render( ) {
 
         return (
-            <div>
-
+            <div className='nav'>
+                <ul>
+                    <li>
+                        <NavLink to='/' activeClassName='active'>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/leaderboard' activeClassName='active'>
+                            Leaderboard
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/new' activeClassName='active'>
+                            New Question
+                        </NavLink>
+                    </li>
+                </ul>
             </div>
         )
     }
